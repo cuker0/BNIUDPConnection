0ba2e62dfaafe316a562ed471341aed9783d4d8c
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2019-11-13 14:44:18 +0100

    IP via Textbox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.IPAdd = new System.Windows.Forms.TextBox();
+            this.Label_IPAddress = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 12);
+            this.button1.Location = new System.Drawing.Point(342, 127);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 145);
+            this.button1.Size = new System.Drawing.Size(153, 145);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,11 +52,29 @@
             this.textBox1.Size = new System.Drawing.Size(324, 426);
             this.textBox1.TabIndex = 1;
             // 
+            // IPAdd
+            // 
+            this.IPAdd.Location = new System.Drawing.Point(343, 34);
+            this.IPAdd.Name = "IPAdd";
+            this.IPAdd.Size = new System.Drawing.Size(152, 20);
+            this.IPAdd.TabIndex = 2;
+            // 
+            // Label_IPAddress
+            // 
+            this.Label_IPAddress.AutoSize = true;
+            this.Label_IPAddress.Location = new System.Drawing.Point(343, 13);
+            this.Label_IPAddress.Name = "Label_IPAddress";
+            this.Label_IPAddress.Size = new System.Drawing.Size(87, 13);
+            this.Label_IPAddress.TabIndex = 3;
+            this.Label_IPAddress.Text = "Label_IPAddress";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(507, 442);
+            this.Controls.Add(this.Label_IPAddress);
+            this.Controls.Add(this.IPAdd);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +88,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox IPAdd;
+        private System.Windows.Forms.Label Label_IPAddress;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,27 +23,67 @@ namespace BNIUDPConnection
         {
             int broadport = 1999;
             int listenport = 2000;
+            string IpAddress = IPAdd.Text;
 
             UdpClient udpBroadcast = new UdpClient(broadport);
-            IPEndPoint groupEp = new IPEndPoint(IPAddress.Parse("192.168.1.2"), broadport);
+            IPEndPoint groupEp = new IPEndPoint(IPAddress.Parse(IpAddress), broadport);
             UdpClient udpRecieve = new UdpClient(listenport);
             IPEndPoint groupREp = new IPEndPoint(IPAddress.Any, listenport);
-         
-            var hex = new byte[] { 0x24, 0x00, 0x02, 0x0f, 0x04, 0x00, 0x0c, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x20 };
+           
+            var hex = new byte[]
+            {
+                0x24, // byte low
+                0x00, // byte high 
+                0x02, // protocol revision
+                0x0f, // command read process data
+                0x04, // port NO
+                0x00, // module detail
+                0x0c, // port configuration
+                0x11, // IOL revision
+                0x00, // Data store
+                0x00, //Reserved
+                0x00, //Reserved
+                0x00, //Reserved
+                0x00, //Insp level
+                0x00, //Config Vendor ID MSB
+                0x00, //Config Vendor ID LSB
+                0x00, //Config Device ID MSB
+                0x00, //Config Device ID LSB
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No 
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No 
+                0x00, //Config Serial No
+                0x00, //Config Serial No  
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x00, //Config Serial No
+                0x20, //Number of Input Bytes
+                0x20  //Number of Input Bytes
+            };
 
-            udpBroadcast.Connect("192.168.1.2", 1999);
+            udpBroadcast.Connect(groupEp.Address,groupEp.Port);
 
             udpBroadcast.Send(hex, hex.Length);
             udpBroadcast.Close();
 
             byte[] rMessage = udpRecieve.Receive(ref groupREp);
-                
-      
+            
             foreach (int i in rMessage)
             {
                 textBox1.AppendText(i.ToString());
             }
-            
+
+
+
             udpRecieve.Close();
       
         }
